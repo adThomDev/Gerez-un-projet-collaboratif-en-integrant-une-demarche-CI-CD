3db79fb42496460e7421faa12ef8b1f4fd923fed
adThomDev <gretatpcda@gmail.com>
2025-05-18 17:07:21 +0200

    Env files implementation

--- front/src/app/services/jokes.service.ts
+++ front/src/app/services/jokes.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { BehaviorSubject, Observable } from 'rxjs';
 import { Joke } from '../model/joke.model';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
