826e18024a87775bec15e903e895ad1097b8f7c2
adThomDev <gretatpcda@gmail.com>
2025-05-25 12:28:50 +0200

    testing

--- front/karma.conf.js
+++ front/karma.conf.js
@@ -30,7 +30,6 @@ module.exports = function (config) {
       reporters: [
         { type: 'html' },
         { type: 'text-summary' },
-        // { type: 'lcovonly', subdir: '.', file: 'lcov.info' }
       ]
     },
     reporters: ['progress', 'kjhtml'],
