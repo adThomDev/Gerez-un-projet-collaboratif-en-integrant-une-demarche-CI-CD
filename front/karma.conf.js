9b76c718cdbef772cee25446402ef4e48432f54f
adThomDev <gretatpcda@gmail.com>
2025-06-02 12:20:36 +0200

    Rollback to original state

--- front/karma.conf.js
+++ front/karma.conf.js
@@ -29,8 +29,7 @@ module.exports = function (config) {
       subdir: '.',
       reporters: [
         { type: 'html' },
-        { type: 'text-summary' },
-        { type: 'lcovonly', subdir: '.', file: 'lcov.info' }
+        { type: 'text-summary' }
       ]
     },
     reporters: ['progress', 'kjhtml'],
